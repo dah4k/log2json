fb3a38e8e5df8a3ab4343a302fd90903254cab6a
dah4k <84741290+dah4k@users.noreply.github.com>
2025-02-02 15:39:58 -0500

    Add function usage()

--- log2json.c
+++ log2json.c
@@ -9,10 +9,34 @@ typedef struct {
     const char *value;
 } KeyValue_t;
 
+void usage(const char* progname)
+{
+    fprintf(stderr,
+"Usage: %s INPUT_LOG_FILE\n"
+"\n"
+"Parse input log file and print the JSON-ified entries to STDOUT.\n"
+"The expected grammar of the input is:\n"
+"\n"
+"    LOG_ENTRY ::= KEY '=' VALUE [ SPACE KEY '=' VALUE ]* NEWLINE\n"
+"    KEY ::= TEXT\n"
+"    VALUE ::= TEXT | '\"' QUOTED_TEXT '\"'\n"
+"    SPACE ::= 0x20\n"
+"    NEWLINE ::= 0x0A\n"
+"    TEXT ::= WORD_CHARACTER+\n"
+"    QUOTED_TEXT ::= [ WORD_CHARACTER | WHITESPACE | '\\\\' | '\\\"' ]+\n"
+"\n"
+"Known Issues:\n"
+"- Not implemented yet.\n"
+"- Quoted text value containing invalid UTF-8 code points.\n",
+    progname);
+}
+
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+    if (argc != 2) {
+        usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     KeyValue_t kv;
     kv.key = "TODO";
